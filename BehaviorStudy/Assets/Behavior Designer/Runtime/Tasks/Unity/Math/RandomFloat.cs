04a24e6e5a049c899f5e9c60a89c234cdda969a1
chiqingpeng <chiqingpeng@mngo.com>
2020-03-26 09:33:22 +0800

    add random example

--- BehaviorStudy/Assets/Behavior Designer/Runtime/Tasks/Unity/Math/RandomFloat.cs
+++ BehaviorStudy/Assets/Behavior Designer/Runtime/Tasks/Unity/Math/RandomFloat.cs
@@ -15,6 +15,18 @@ namespace BehaviorDesigner.Runtime.Tasks.Unity.Math
         [Tooltip("The variable to store the result")]
         public SharedFloat storeResult;
 
+#region add new code
+        public GameObject obj;
+        public BehaviorTree bt;
+        private float originY = 0;
+		public override void OnStart()
+		{
+			SharedGameObject sobj = bt.GetVariable("pop_prefab") as SharedGameObject;
+			obj = GameObject.Instantiate(sobj.Value);
+            if(obj)
+                originY = obj.transform.position.y;
+		}
+#endregion
         public override TaskStatus OnUpdate()
         {
             if (inclusive) {
@@ -22,7 +34,20 @@ namespace BehaviorDesigner.Runtime.Tasks.Unity.Math
             } else {
                 storeResult.Value = Random.Range(min.Value, max.Value - 0.00001f);
             }
-            return TaskStatus.Success;
+#region add new code
+            if(obj)
+            {
+                obj.transform.position = Vector3.MoveTowards(obj.transform.position, new Vector3(obj.transform.position.x, obj.transform.position.y + storeResult.Value, obj.transform.position.z), 2 * Time.deltaTime);
+                if(Mathf.Abs(obj.transform.position.y - originY) < 3)
+                    return TaskStatus.Running;
+                else
+                {
+                    GameObject.Destroy(obj);
+                    return TaskStatus.Success;
+                }
+            }
+            return TaskStatus.Failure;
+#endregion
         }
 
         public override void OnReset()
